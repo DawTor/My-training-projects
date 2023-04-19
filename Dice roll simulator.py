1e14591196f7e36597567ded5d85333229d879d4
DawTor <111532769+DawTor@users.noreply.github.com>
2023-04-19 20:22:55 +0200

    Update Dice roll simulator.py
    Added an easier way to write my code

--- Dice roll simulator.py
+++ Dice roll simulator.py
@@ -1,13 +1,22 @@
 import random
 
-while True:
+if __name__ == '__main__':
+    while True:
+        question = input("Do you want to roll dice?(yes/no): ")
 
-    roll = random.randint(1, 6)
+        if question == "yes":
+            roll = random.randint(1, 6)
+            print(f"Dice shows {roll}")
 
-    question = input("Do you want to roll dice?(yes/no): ")
+        else:
+            break
+    print("Bye!")
 
-    if question == "yes":
-        print(f"Dice shows {roll}")
-    else:
-        break
-print("Bye!")
+#Easier way to write this code (Thanks to https://github.com/DanielChabrowski)
+#import random
+
+#if __name__ == '__main__':
+#   while input('Do you want to roll dice?(yes/no): ') == 'yes':
+#       roll = random.randint(1, 6)
+#       print(f"Dice shows {roll}")
+#   print("Bye!")
